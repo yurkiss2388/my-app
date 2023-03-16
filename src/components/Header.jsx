cb63f3ed92b2948cad1022bbbd1a98ff94d99f05
yurkiss2388 <urahudakov0@gmail.com>
2023-03-16 10:52:11 +0300

    разбил на компаненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://logoza.ru/img/rock.png' />
+    </header>
+}
+
+export default Header;
