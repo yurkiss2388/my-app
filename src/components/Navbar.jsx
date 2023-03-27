ee9a764065df87857149d14517f0cb8c154113d1
yurkiss2388 <urahudakov0@gmail.com>
2023-03-27 11:54:44 +0300

    exapmle commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import './Navbar.css';
 const Navbar = () => {
     return <nav className='nav'>
-        <div className='item'>
+        <div className='nav__item'>
             <a>Profile</a>
         </div>
-        <div className='item'>
+        <div className='nav__item'>
             <a>Messeges</a>
         </div>
-        <div className='item'>
+        <div className='nav__item'>
             <a>News</a>
         </div>
-        <div className='item'>
+        <div className='nav__item'>
             <a>Music</a>
         </div >
-        <div className='item'>
+        <div className='nav__item'>
             <a>Settings</a>
         </div>
     </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import './Profile.css';
 const Profile = () => {
     return <div className='profile'>
         <div>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1hazgLb0mA30SqlKhlb06Lsa1Tq8xcFhuElXyog5Ypi7tIyr6kufpZn7xhZUbh6ci5t0&usqp=CAU' />;
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1hazgLb0mA30SqlKhlb06Lsa1Tq8xcFhuElXyog5Ypi7tIyr6kufpZn7xhZUbh6ci5t0&usqp=CAU' />
         </div>
         
         <div className='item'>
